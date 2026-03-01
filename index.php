86708b7175da59906258ed7d62e2dff45600f760
Tobias Möritz <tobias@moeritz.io>
2026-03-01 22:53:29 +0100

    fix: properly load all classes on non-composer environments

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php
 
 load([
-	'Kirby\Cache\StatiCache' => __DIR__ . '/src/StatiCache.php'
+	'Kirby\Cache\StatiCache' => __DIR__ . '/src/StatiCache.php',
+	'Kirby\Cache\Compression\BrotliEncoder' => __DIR__ . '/src/Compression/BrotliEncoder.php',
+	'Kirby\Cache\Compression\CompressionEncoder' => __DIR__ . '/src/Compression/CompressionEncoder.php',
+	'Kirby\Cache\Compression\GzipEncoder' => __DIR__ . '/src/Compression/GzipEncoder.php',
+	'Kirby\Cache\Compression\ZstdEncoder' => __DIR__ . '/src/Compression/ZstdEncoder.php'
 ]);
 
 Kirby::plugin('getkirby/staticache', [
